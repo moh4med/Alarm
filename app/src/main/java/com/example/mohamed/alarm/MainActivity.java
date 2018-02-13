4c2d09e183839aaec66733f6b963d641923f858c
mohamed <moh4med.ahmed@gmail.com>
2018-02-13 17:19:38 +0200

    setup the UI.

--- app/src/main/java/com/example/mohamed/alarm/MainActivity.java
+++ app/src/main/java/com/example/mohamed/alarm/MainActivity.java
@@ -1,13 +1,71 @@
 package com.example.mohamed.alarm;
 
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements AlarmFragment.OnFragmentInteractionListener
+        ,TimerFragment.OnFragmentInteractionListener
+        ,StopWatchFragment.OnFragmentInteractionListener{
+    private final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ActionBar actionBar = getSupportActionBar();
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        viewPager.setAdapter(new MyPageAdapter(getSupportFragmentManager()));
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    class MyPageAdapter extends FragmentPagerAdapter{
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
+        public MyPageAdapter(FragmentManager fm) {
+            super(fm);
+            addFragment(AlarmFragment.newInstance("alarm","fragment"),"Alarm");
+            addFragment(TimerFragment.newInstance("timer","fragment"),"Timer");
+            addFragment(StopWatchFragment.newInstance("stopwatch","fragment"),"StopWatch");
+        }
+
     }
 }
